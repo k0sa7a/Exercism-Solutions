74d2a0595af1924fe08a2e524363e4c24707ce60
Stefan Filev <stefan.s.filev@gmail.com>
2022-07-10 14:28:20 +0100

    Twofer String exercise

---
+++ Twofer/Twofer.java
@@ -0,0 +1,9 @@
+public class Twofer {
+  public String twofer(String name) {
+      if(name != null && name != ""){
+          return String.format("One for %s, one for me.", name);
+      } else {
+          return "One for you, one for me.";
+      }
+  }
+}
